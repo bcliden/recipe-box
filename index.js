25d20354cac67b3a9932ccd7c89b82501d85ac4f
Benjamin Liden <benjamin.c.liden@gmail.com>
2018-02-19 19:06:08 -0500

    css linked and ejs working

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const ejs = require('ejs');
+
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+    res.render("partials/header");
+})
+
+const port = process.env.port || 7575;
+app.listen(port, () => {
+    console.log('The app is running on port ', port)
+});
