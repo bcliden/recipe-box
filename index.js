601d1e836b78b67261aec6699010c2cbce7880a9
Benjamin Liden <benjamin.c.liden@gmail.com>
2018-02-26 16:42:03 -0500

    mock db object added for templating && card borders added

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
 const express = require('express');
 const ejs = require('ejs');
 
+const seedRecipes = [
+    {
+        title: 'Pear brandy',
+        author: 'Ben',
+        description: 'A very tasty pear brandy! Of roman and scotch heritage. What a mystery.'
+    },
+    {
+        title: 'Lemon wedges',
+        author: 'Medieval Man',
+        description: 'it\'s sugared lemon wedges. big surprise.'
+    },
+    {
+        title: 'Mac \'n\' Cheese',
+        author: 'Bridget Sellers',
+        description: 'All I know is pain. Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni voluptatibus dolorum alias repellendus culpa, quam unde id velit. Sint, consequatur!'
+    },
+    {
+        title: 'Pot of Rice, perfect',
+        author: 'Bon Appetit',
+        description: 'Literally the most perfect pot of rice. Honed after several decades.'
+    }
+]
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -12,7 +34,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/recipes', (req, res) => {
-    res.render('index');
+    res.render('index', { recipes: seedRecipes });
 });
 
 app.post('/recipes', (req, res) => {
