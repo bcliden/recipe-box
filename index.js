c88cbf45e9bfbb559313c10d9b0dd23bb934b4e5
Benjamin Liden <benjamin.c.liden@gmail.com>
2018-02-26 13:11:29 -0500

    basic form in place. no ingred or steps

--- index.js
+++ index.js
@@ -8,8 +8,16 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
+    res.redirect('/recipes');
+});
+
+app.get('/recipes', (req, res) => {
     res.render('index');
 });
+
+app.post('/recipes', (req, res) => {
+    res.send('this is the post rt');
+})
 
 app.get('/new', (req, res) => {
     res.render('new');
