d854b45ae13d0d26fac5b448a89d74a1ccfa7a15
Benjamin Liden <benjamin.c.liden@gmail.com>
2018-02-27 15:33:08 -0500

    dummy db data added. steps and ingredients pages added. root font changed.

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,5 +1,22 @@
-const menuButton = document.querySelector('.menu-button');
+const menuBtn = document.querySelector('.menu-button');
+const ingredientsBtn = document.querySelector('.more-ingredients');
+const stepsBtn = document.querySelector('.more-steps');
 
-menuButton.addEventListener('click', () => {
+menuBtn.addEventListener('click', () => {
     document.querySelector('.menu').classList.toggle('menu-visible');
-})
+});
+
+ingredientsBtn.addEventListener('click', () => {
+    let ingredients = document.querySelector('.ingredients');
+    let elem = document.createElement('input');
+    elem.setAttribute('type', 'text');
+    elem.setAttribute('name', 'ingredients[]');
+    ingredients.appendChild(elem);
+});
+
+stepsBtn.addEventListener('click', () => {
+    let steps = document.querySelector('.steps');
+    let elem = document.createElement('div');
+    elem.innerHTML = `<textarea name="steps[]" cols="10" rows="5"></textarea>`;
+    steps.appendChild(elem);
+});
