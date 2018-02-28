e9a7a3da0490af81b2c83624bb0575e6c7b47e0e
Benjamin Liden <benjamin.c.liden@gmail.com>
2018-02-27 20:49:09 -0500

    new pg buttons conditional. featured card font sizes tweaked (smaller)

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -6,17 +6,21 @@ menuBtn.addEventListener('click', () => {
     document.querySelector('.menu').classList.toggle('menu-visible');
 });
 
-ingredientsBtn.addEventListener('click', () => {
-    let ingredients = document.querySelector('.ingredients');
-    let elem = document.createElement('input');
-    elem.setAttribute('type', 'text');
-    elem.setAttribute('name', 'ingredients[]');
-    ingredients.appendChild(elem);
-});
+if (ingredientsBtn) {
+    ingredientsBtn.addEventListener('click', () => {
+        let ingredients = document.querySelector('.ingredients');
+        let elem = document.createElement('input');
+        elem.setAttribute('type', 'text');
+        elem.setAttribute('name', 'ingredients[]');
+        ingredients.appendChild(elem);
+    });
+};
 
-stepsBtn.addEventListener('click', () => {
-    let steps = document.querySelector('.steps');
-    let elem = document.createElement('div');
-    elem.innerHTML = `<textarea name="steps[]" cols="10" rows="5"></textarea>`;
-    steps.appendChild(elem);
-});
+if (stepsBtn) {
+    stepsBtn.addEventListener('click', () => {
+        let steps = document.querySelector('.steps');
+        let elem = document.createElement('div');
+        elem.innerHTML = `<textarea name="steps[]" cols="10" rows="5"></textarea>`;
+        steps.appendChild(elem);
+    });
+};
