9fac0eaa822a825604d5c9267eb5597aa688ca4f
Benjamin Liden <benjamin.c.liden@gmail.com>
2018-02-28 13:43:24 -0500

    create and find routes working successfully

--- routes/recipes.js
+++ routes/recipes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../models');
+const Recipe = db.Recipe;
 const seedRecipes = require('../seedRecipes');
 
 router.get('/', (req, res) => {
@@ -8,22 +9,27 @@ router.get('/', (req, res) => {
 });
 
 router.get('/recipes', (req, res) => {
-    res.render('index', { recipes: seedRecipes });
+    Recipe.find({})
+        .then( foundRecipes => {
+            res.render('index', { recipes: foundRecipes });
+        })
+        .catch( err => console.err(err));
 });
 
 router.post('/recipes', (req, res) => {
     let { title, author, description, ingredients, steps } = req.body;
     ingredients = ingredients.filter(recipe => recipe.trim().length > 0);
     steps = steps.filter(step => step.trim().length > 0);
-    const newRecipe = {
+    
+    Recipe.create({
         title,
         author,
         description,
         ingredients,
         steps
-    };
-    console.log(newRecipe);
-    res.redirect('/recipes');
+    })
+        .then( () => res.redirect('/recipes') )
+        .catch( err => console.err(err) );
 });
 
 router.get('/new', (req, res) => {
