fdffe9884349e1489a4c8dd28e024e63821dfbfa
Benjamin Liden <benjamin.c.liden@gmail.com>
2018-03-01 23:12:15 -0500

    Card margin updated. Submit -> Save on EDIT, mobile font shrunk. All routes working

--- index.js
+++ index.js
@@ -5,16 +5,6 @@ const methodOverride = require('method-override');
 const recipeRoutes = require('./routes/recipes');
 
 app.use(express.urlencoded({ extended: true }));
-
-// hacky way to make DELETE request with the SHOW route <a> tag
-app.use('/recipes/:recipeId', ( req, res, next ) => {
-    if ( req.query._method == 'DELETE' ) {
-        req.method = 'DELETE';
-        req.url = req.path;
-    }       
-    next(); 
-});
-
 app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
