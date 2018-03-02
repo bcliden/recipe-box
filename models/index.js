8ff84dea61491e111b630248ff52f881c93a7348
Benjamin Liden <benjamin.c.liden@gmail.com>
2018-03-02 12:16:54 -0500

    handled db connection error better

--- models/index.js
+++ models/index.js
@@ -4,13 +4,15 @@ const uri = 'mongodb://localhost/recipe-box';
 mongoose.Promise = global.Promise;
 mongoose.set('debug', true);
 
-mongoose.connect(uri);
+mongoose.connect(uri)
+    .catch(err => console.error(err.message));
 mongoose.connection
     .on('error', (err) => {
         console.error.bind(console, 'Connection error:');
     })
     .once('open', () => {
         console.log('DB connected successfully.');
-    });
+    })
+    .catch(err => console.error(err.message));
 
 module.exports.Recipe = require('./recipe');
