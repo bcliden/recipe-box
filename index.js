dfc8cebedbaaaa8cf1f60b6610deb13273b685f1
Benjamin Liden <benjamin.c.liden@gmail.com>
2018-03-07 16:50:13 -0500

    app index reorganized

--- index.js
+++ index.js
@@ -4,13 +4,18 @@ const ejs = require('ejs');
 const methodOverride = require('method-override');
 const recipeRoutes = require('./routes/recipes');
 
-app.use(express.urlencoded({ extended: true }));
-app.use(methodOverride('_method'));
+// EXPRESS CONFIG
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
+// PASSPORT CONFIG
+
+// ROUTING
 app.use('/', recipeRoutes);
 
+// SERVER CONFIG
 const port = process.env.port || 7575;
 app.listen(port, () => {
     console.log('The app is running on port ', port)
