c392151790c3cb96d1ac98a9227b9976ec1d2d5e
Benjamin Liden <benjamin.c.liden@gmail.com>
2018-03-16 12:36:26 -0400

    title field in NEW autofocus. Comment on schema

--- models/recipe.js
+++ models/recipe.js
@@ -26,6 +26,8 @@ const recipeSchema = new Schema({
 });
 
 // VALIDATION FNs obsolete thanks to required
+// this works well with the arrays thanks to trim middleware
+// which nulls empty arrays
 
 // function notEmpty(value) {
 //     return value.trim().length > 0;
