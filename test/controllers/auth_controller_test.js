0bd9b74794270df6ecd7f56a659432cd1cd04a1b
Benjamin Liden <benjamin.c.liden@gmail.com>
2018-04-19 13:44:24 -0400

    tests for bad auth requests

--- test/controllers/auth_controller_test.js
+++ test/controllers/auth_controller_test.js
@@ -17,6 +17,22 @@ describe('Authentication Controllers', () => {
                         assert( count + 1 === newCount);
                         done();
                     })
+                    .catch( err => done(err))
+                })
+        }).catch( err => console.error(err))
+    });
+    it('Will not register an incomplete request', (done) => {
+        User.count().then( count => {
+            request(app)
+                .post('/register')
+                .send({username: 'test'})
+                .end( (err, res) => {
+                    if(err){ return done(err) };
+                    User.count().then( newCount => {
+                        assert( count === newCount);
+                        done();
+                    })
+                    .catch( err => done(err));
                 })
         }).catch( err => console.error(err))
     });
@@ -36,6 +52,16 @@ describe('Authentication Controllers', () => {
                 request(app)
                     .post('/login')
                     .send({username: 'test', password: 'badpwd'})
+                    .expect(302)
+                    .expect('Location', '/login', done)
+            })
+    });
+    it('Does not log in a bad username', function(done) {
+        User.register({username: 'test'}, '123456')
+            .then( () => {
+                request(app)
+                    .post('/login')
+                    .send({username: 'badusr', password: '123456'})
                     .expect(302)
                     .expect('Location', '/login', done)
             })
